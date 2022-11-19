6dee1e4921374ac03a15b14fec0cd72cfc3e9f60
Dominik Małkowiak <dominik.malkowiak@gmail.com>
2022-11-19 23:28:55 +0100

    tmp work around

--- pages/api/description.js
+++ pages/api/description.js
@@ -1,6 +1,20 @@
-export default (req, res) => {
+import {pool} from '../../helpers/pg';
+import {validateToken} from '../../helpers/jwt'
+import {v4 as uuid} from 'uuid';
+
+export default async (req, res) => {
+    console.log('123')
+    let userId = null;
+    if(!(userId = validateToken(req))) return res.status(403).send();
+
+    // TODO: validation
     console.log(req.body);
+    try {
+        await pool.query('INSERT INTO products (id, user_id, title) VALUES ($1, $2, $3);', [uuid(), userId, req.body.title]);
+        
+    } catch (err) {
+        console.log(err);
+    }
 
-
-    res.status(201).send();
+    console.log(req.cookies['token']);
 }
