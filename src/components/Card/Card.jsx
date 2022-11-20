2dc1197b7216e0a5eab19eec5d5f798ab07ea9ef
Marcin Krueger <marcinkrueger@gmail.com>
2022-11-20 08:29:02 +0100

    I dont know what i am doing

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,16 @@
 import styles from "./Card.module.css";
 import classNames from "classnames";
 
-export const Card = function ({ image, title, description, id, className }) {
+export const Card = function ({
+  image,
+  title,
+  description,
+  id,
+  style,
+  className,
+}) {
   return (
-    <div id={id} className={classNames(styles.card, className)}>
+    <div id={id} className={classNames(styles.card, className)} style={style}>
       <img src={image} alt="Item" className={styles.cardImage} />
       <div className={styles.textContent}>
         <h2 className={styles.itemTitle}>{title}</h2>
--- src/pages/Swipe/Swipe.jsx
+++ src/pages/Swipe/Swipe.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Nav } from "src/components/Nav/Nav";
 import { Card } from "src/components/Card/Card";
 import { Controls } from "src/components/Controls/Controls";
@@ -10,7 +10,8 @@ export const Swipe = function () {
     {
       image: "images/lamp.jpeg",
       title: "Stara lampa",
-      description: "Fajna lampa, nie działa text text text text text text text text text text",
+      description:
+        "Fajna lampa, nie działa text text text text text text text text text text",
     },
     {
       image: "images/lamp.jpeg",
@@ -26,29 +27,60 @@ export const Swipe = function () {
 
   const [indexCard, setIndexCard] = useState(0);
 
+  useEffect(() => {
+    fetch("/api/products")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(cardBuffer);
+        setCardBuffer((cardBuffer) =>
+          cardBuffer.concat(
+            res.map((el) => {
+              return {
+                image: el.images[0],
+                title: el.title,
+                description: el.description,
+              };
+            })
+          )
+        );
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <>
       <Nav />
-      <div className={styles.content}>
-        <Buffer infoBuffer={cardBuffer} />
-        <Controls
-          rightOnClick={() => {
-            const first = document.querySelectorAll("." + styles.gridElement)[indexCard];
-            first.classList.add(styles.swipeRight);
+      {cardBuffer && cardBuffer.length > 0 ? (
+        <div className={styles.content}>
+          <Buffer infoBuffer={cardBuffer} />
+          <Controls
+            rightOnClick={() => {
+              if (indexCard < cardBuffer.length) {
+                const first = document.querySelectorAll(
+                  "." + styles.gridElement
+                )[indexCard];
+                first.classList.add(styles.swipeRight);
 
-            setIndexCard(indexCard + 1);
-            setTimeout(() => first.classList.add(styles.none), 2000);
-          }}
+                setIndexCard(indexCard + 1);
+                setTimeout(() => first.classList.add(styles.none), 2000);
+              }
+            }}
+            leftOnClick={() => {
+              if (indexCard < cardBuffer.length) {
+                const first = document.querySelectorAll(
+                  "." + styles.gridElement
+                )[indexCard];
+                first.classList.add(styles.swipeLeft);
 
-          leftOnClick={() => {
-            const first = document.querySelectorAll("." + styles.gridElement)[indexCard];
-            first.classList.add(styles.swipeLeft);
-
-            setIndexCard(indexCard + 1);
-            setTimeout(() => first.classList.add(styles.none), 2000);
-          }}
-        />
-      </div>
+                setIndexCard(indexCard + 1);
+                setTimeout(() => first.classList.add(styles.none), 2000);
+              }
+            }}
+          />
+        </div>
+      ) : (
+        <p>XDDDDDD</p>
+      )}
     </>
   );
 };
@@ -56,16 +88,18 @@ export const Swipe = function () {
 const Buffer = function ({ infoBuffer }) {
   return (
     <div className={styles.bufferContainer}>
+      {console.log(infoBuffer)}
       {infoBuffer &&
+        infoBuffer.length > 0 &&
         infoBuffer?.map((el, index) => (
           <Card
             image={el.image}
             title={el.title}
             description={el.description}
             className={classNames(
-              styles.gridElement,
-              styles[`visibility-${index}`]
+              styles.gridElement
             )}
+            style={{zIndex: -index}}
             id={index === 0 ? "firstCard" : undefined}
             key={index}
           />
