fcaf1529bebf71b94e3b13e39fefb9708c2a3405
Vadym Repetskyi <vadym.repetskyi@gmail.com>
2022-11-20 11:57:31 +0100

    Removed bugs

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -8,17 +8,6 @@ export default function Profile({ name, surname, email, address, products }) {
       <p>{surname}</p>
       <p>{email}</p>
       <p>{address}</p>
-      <div>
-        {products.forEach((product) => (
-          <div>
-            {product.images.length && (
-              <img src={product.images[0]} alt={product.title} />
-            )}
-            <p>{product.title}</p>
-            <p>{product.description}</p>
-          </div>
-        ))}
-      </div>
       <Link href="/upload">Add product</Link>
     </div>
   );
